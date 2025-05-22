22c58e6e4214a55f7e1b1ef55f4ccef0b34abe94
Shomurotov Umarbek <shumarbek001@gmail.com>
2025-05-22 17:00:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ let i = 0;
 next.onclick = function () {
     img[i].style.display = 'none';
     i++;
-    if (i >= 7) {
-    //sliderda 8 ta rasm bolganligi uchun i>=7 sharti qoyildi(index 0 dan boshlanadi) / The condition i>=7 was set because there are 8 images in the slider (index starts from 0)
+    if (i >= img.length) {
         i = 0
         img[i].style.display = 'block';
     } else {
@@ -20,7 +19,6 @@ prev.onclick = function () {
     i--;
     if (i < 0) {
         i = 7;
-      //sliderda 8 ta rasm bolganligi uchun i>=7 sharti qoyildi(index 0 dan boshlanadi) / The condition i>=7 was set because there are 8 images in the slider (index starts from 0)
         img[i].style.display = 'block';
     } else {
         img[i].style.display = 'block';
