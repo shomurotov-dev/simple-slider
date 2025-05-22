79962d5d1b0b95e7e49b5d15cbcc481605a7c4e7
Shomurotov Umarbek <shumarbek001@gmail.com>
2025-05-22 16:58:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ let i = 0;
 next.onclick = function () {
     img[i].style.display = 'none';
     i++;
-    if (i >= 7) {    //sliderda 8 ta rasm bolganligi uchun i>=7 sharti qoyildi(index 0 dan boshlanadi) / The condition i>=7 was set because there are 8 images in the slider (index starts from 0)
+    if (i >= 7) {
+    //sliderda 8 ta rasm bolganligi uchun i>=7 sharti qoyildi(index 0 dan boshlanadi) / The condition i>=7 was set because there are 8 images in the slider (index starts from 0)
         i = 0
         img[i].style.display = 'block';
     } else {
@@ -17,8 +18,9 @@ next.onclick = function () {
 prev.onclick = function () {
     img[i].style.display = 'none';
     i--;
-    if (i <= 0) {
-        i = 7;      //sliderda 8 ta rasm bolganligi uchun i>=7 sharti qoyildi(index 0 dan boshlanadi) / The condition i>=7 was set because there are 8 images in the slider (index starts from 0)
+    if (i < 0) {
+        i = 7;
+      //sliderda 8 ta rasm bolganligi uchun i>=7 sharti qoyildi(index 0 dan boshlanadi) / The condition i>=7 was set because there are 8 images in the slider (index starts from 0)
         img[i].style.display = 'block';
     } else {
         img[i].style.display = 'block';
